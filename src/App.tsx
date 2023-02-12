f684966b96dd652526aac63e567eb06c9c588af9
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2023-02-12 12:04:02 +0530

    Commit 2 Added ghPages requirements.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './Components/Header';
 function App() {
   return (
     <>
-      <Box height='100vh' width='100vw' display='flex' justifyContent='center' alignItems='center'>
+      <Box height='100vh' width='100vw' display='flex' justifyContent='center' alignItems='center' sx={{ display: { xs: 'none', lg: 'block' } }}>
         <Stack height='100%' width='100%' direction='column' justifyContent='center' alignItems='center'>
           {/* Header */}
           <Box height='9%' width='100%' display='flex' justifyContent='center' alignItems='center' bgcolor='#ffffff'>
@@ -13,7 +13,19 @@ function App() {
           </Box>
           {/* Content */}
           <Box height='91%' width='100%' display='flex' justifyContent='center' alignItems='center' bgcolor='#edeff2'>
-            <Content/>
+            <Content />
+          </Box>
+        </Stack>
+      </Box>
+      <Box height='768px' width='1366px' display='flex' justifyContent='center' alignItems='center' sx={{ display: { xs: 'block', lg: 'none' } }}>
+        <Stack height='100%' width='100%' direction='column' justifyContent='center' alignItems='center'>
+          {/* Header */}
+          <Box height='9%' width='100%' display='flex' justifyContent='center' alignItems='center' bgcolor='#ffffff'>
+            <Header />
+          </Box>
+          {/* Content */}
+          <Box height='91%' width='100%' display='flex' justifyContent='center' alignItems='center' bgcolor='#edeff2'>
+            <Content />
           </Box>
         </Stack>
       </Box>
